c0381727c3832177dbae3fe29648ee761211d359
joseph085 <126801512+joseph085@users.noreply.github.com>
2023-05-08 13:09:52 +0400

    Add files via upload

---
+++ The_Last_13_Lessons/The_Last_13_Lessons/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace The_Last_13_Lessons
+{
+    internal class Program
+    {
+        public class Employee
+        {
+            public string _firstName;
+            public string _lastName;
+            public string _fatherName;
+            public int _age;
+            public string _gender;
+            public string _birthday;
+            public string _city;
+
+
+        }
+        public static void Main(string[] args)
+        {
+            #region 18.Has-a-RelationShip
+            Employee employee = new Employee
+            {
+                _firstName = "Yusif",
+                _lastName = "Huseynzade",
+                _fatherName = "Qulu",
+                _age = 25,
+                _gender = "Male",
+                _birthday = "26.08.1998"
+            };
+            Console.WriteLine(employee._firstName);
+            Console.WriteLine(employee._lastName);
+            Console.WriteLine(employee._fatherName);
+            Console.WriteLine(employee._age);
+            Console.WriteLine(employee._gender);
+            Console.WriteLine(employee._birthday);
+
+
+            #endregion 
+
+
+
+
+
+
+        }
+
+    }
+}
