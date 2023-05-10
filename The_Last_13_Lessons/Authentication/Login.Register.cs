fc44b9db4f1c5ec5b42a7fb81e6ad8455df543f4
joseph085 <126801512+joseph085@users.noreply.github.com>
2023-05-10 22:42:43 +0400

    Login and Register with switch keys

---
+++ The_Last_13_Lessons/Authentication/Login.Register.cs
@@ -0,0 +1,63 @@
+﻿namespace Authentication
+{
+    class User
+    {
+        public string _name;
+        public string _lastName;
+        public string _password;
+        public string _email;
+
+        public User(string name, string lastName, string password, string email)
+        {
+            _name = name;
+            _lastName = lastName;
+            _password = password;
+            _email = email;
+        }
+    }
+
+    class LoginCommand
+    {
+        public void Handle()
+        {
+
+        }
+    }
+
+    class RegisterCommand
+    {
+        public void Handle()
+        {
+
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string command = Console.ReadLine()!; //==> ! isaresini ona gore sona atiriqki eksi olsun diye
+                switch (command)
+                {
+                    case "/register":
+                        RegisterCommand registerCommand = new RegisterCommand();
+                        registerCommand.Handle();
+                        break;
+                    case "/login":
+                        LoginCommand loginCommand = new LoginCommand();
+                        loginCommand.Handle();
+                        break;
+                    case "/exit":
+                        Console.WriteLine("Bye-bye");
+                        return;
+                  default:
+                        Console.WriteLine("Invalid command pls try again :");
+                        break;
+                }
+
+            }
+        }
+    }
+}
--- The_Last_13_Lessons/The_Last_13_Lessons/Program.cs
+++ The_Last_13_Lessons/The_Last_13_Lessons/Program.cs
@@ -1,6 +1,18 @@
 ﻿namespace The_Last_13_Lessons
 {
     public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+
+
+
+
+    }
 
    
 }
