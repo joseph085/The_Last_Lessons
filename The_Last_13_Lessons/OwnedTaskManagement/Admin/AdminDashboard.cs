6ef3acdcfb1958b6e36cbd32c785546c898e887d
joseph085 <126801512+joseph085@users.noreply.github.com>
2023-05-12 04:18:04 +0400

    Owned task AdminDashboard for classes

--- The_Last_13_Lessons/OwnedTaskManagement/Admin/AdminDashboard.cs
+++ The_Last_13_Lessons/OwnedTaskManagement/Admin/AdminDashboard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OwnedTaskManagement.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace OwnedTaskManagement.Admin
 {
-    internal class AdminDashboard
+    public class AdminDashboard
     {
+        public static void İntroduction()
+        {
+            Console.WriteLine("Hello dear Admin");
+            while (true)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "/show-users":
+                        ShowUsersCommands.Handle();
+                        break;
+                    case "/show-user-by-email":
+                        ShowUserByEmailCommand.Handle();
+                        break;
+                    case "/show-user-by-id":
+                        ShowUserByIdCommand.Handle();
+                        break;
+                    case "/add-user":
+                        AddUserCommand.Handle();
+                        break;
+                    case "/logout":
+                        Console.WriteLine("Bye-bye");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid command,pls try again");
+                        break;
+                }
+            }
+        }
     }
 }
